c4df5ae5b1506aa498fad517aca5c0aa23be3bd5
Pavipollo17 <99996169+PabloGonzalezArroyoo@users.noreply.github.com>
2023-12-15 14:22:32 +0100

    Nada nuevo

---
+++ Práctica 2/ProyectosOGREvc15x86/media/IG2App/HolesFS.glsl
@@ -0,0 +1,17 @@
+
+#version 330 core
+in vec2 vUv0;
+
+uniform sampler2D texFront;
+uniform sampler2D texBack;
+
+out vec4 fFragColor;
+
+void main() {
+	vec3 texCoord = vec3(texture(texFront, vUv0));
+	vec3 color = texCoord;
+	if (texCoord.r > 0.6) {
+		color = vec3(texture(texBack, vUv0));
+	}
+	fFragColor = vec4(color, 1.0);
+}
